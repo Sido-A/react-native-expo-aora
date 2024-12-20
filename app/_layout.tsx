ab163075eab84d3c6ccd867bda7bbeb14febea4e
Sido-A <sidonie.abe.uk@gmail.com>
2024-12-20 21:40:10 +0000

    Add other route to hide header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,8 +28,10 @@ const RootLayout = () => {
   return (
     <>
       <Stack>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="/serach/[query]" options={{ headerShown: false }} /> */}
       </Stack>
     </>
   );
